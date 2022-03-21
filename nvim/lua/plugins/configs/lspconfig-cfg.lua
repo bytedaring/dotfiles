195daca757a4dfc86098a31d04ed1033e6247e2a
xiwang <daring@outlook.com>
2022-03-21 11:33:10 +0800

    1, 更新style

--- nvim/lua/plugins/configs/lspconfig-cfg.lua
+++ nvim/lua/plugins/configs/lspconfig-cfg.lua
@@ -12,41 +12,41 @@ M.setup = function()
   
   -- after the language server attaches to the current buffer
   local on_attach = function(client, bufnr)
-      local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-      local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  
-      -- Enable completion triggered by <c-x><c-o>
-      buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-  
-      -- Mappings.
-      local opts = { noremap=true, silent=true }
-      -- Se `:help vim.lsp.*` for documentation on any of the below functions
-      buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-      buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-      buf_set_keymap('n', 'ga', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-      -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-      buf_set_keymap('n', 'gr', '<cmd>TroubleToggle lsp_references<CR>', opts)
-      buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-      buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-      buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-      buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-      buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-      buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-      buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-      buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-      buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-      buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-      buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-      buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-      buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-  
-      -- 保存文件时自动 formatting
-      if client.resolved_capabilities.document_formatting then
-          vim.api.nvim_command [[augroup Format]]
-          vim.api.nvim_command [[autocmd! * <buffer>]]
-          vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-          vim.api.nvim_command [[augroup END]]
-      end
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+    -- Mappings.
+    local opts = { noremap=true, silent=true }
+    -- Se `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'ga', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    -- buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>TroubleToggle lsp_references<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+
+    -- 保存文件时自动 formatting
+    if client.resolved_capabilities.document_formatting then
+      vim.api.nvim_command [[augroup Format]]
+      vim.api.nvim_command [[autocmd! * <buffer>]]
+      vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
+      vim.api.nvim_command [[augroup END]]
+    end
   end
   
   -- Use a loop to conveniently call 'setup' on multiple servers and
@@ -63,7 +63,7 @@ M.setup = function()
   
   -- pyright
   nvim_lsp.pyright.setup{
-      cmd = { "pyright-langserver", "--stdio" },
+    cmd = { "pyright-langserver", "--stdio" },
   	-- for postfix snippets and analyzers
   	capabilities = capabilities,
       filetypes = { "python" },
@@ -71,16 +71,16 @@ M.setup = function()
       --    return M.search_ancestors(startpath, matcher)
       -- end,
       settings = {
-          python = {
-              analyzers = {
-                      autoSearchPaths = true,
-                      diagnosticMode = "workspace",
-                      useLibraryCodeForTypes = true
-              }
+        python = {
+          analyzers = {
+            autoSearchPaths = true,
+            diagnosticMode = "workspace",
+            useLibraryCodeForTypes = true
           }
+        }
       },
   	on_attach = on_attach,
-      single_file_support = false
+    single_file_support = false
   }
   
   -- gopls
@@ -89,81 +89,81 @@ M.setup = function()
   	-- for postfix snippets and analyzers
   	capabilities = capabilities,
   	settings = {
-  	    gopls = {
-  		    experimentalPostfixCompletions = true,
-  		    analyses = {
-  		        unusedparams = true,
-  		        shadow = true,
-  		    },
-  		    staticcheck = true,
-  		    },
-  	    },
+      gopls = {
+        experimentalPostfixCompletions = true,
+        analyses = {
+            unusedparams = true,
+            shadow = true,
+        },
+        staticcheck = true,
+      },
+    },
   	on_attach = on_attach,
   }
   
   function goimports(timeoutms)
-      local context = { source = { organizeImports = true } }
-      vim.validate { context = { context, "t", true } }
-  
-      local params = vim.lsp.util.make_range_params()
-      params.context = context
-  
-      -- See the implementation of the textDocument/codeAction callback
-      -- (lua/vim/lsp/handler.lua) for how to do this properly.
-      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
-      if not result or next(result) == nil then return end
-      local actions = result[1].result
-      if not actions then return end
-      local action = actions[1]
-  
-      -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
-      -- is a CodeAction, it can have either an edit, a command or both. Edits
-      -- should be executed first.
-      if action.edit or type(action.command) == "table" then
-        if action.edit then
-          vim.lsp.util.apply_workspace_edit(action.edit)
-        end
-        if type(action.command) == "table" then
-          vim.lsp.buf.execute_command(action.command)
-        end
-      else
-        vim.lsp.buf.execute_command(action)
+    local context = { source = { organizeImports = true } }
+    vim.validate { context = { context, "t", true } }
+
+    local params = vim.lsp.util.make_range_params()
+    params.context = context
+
+    -- See the implementation of the textDocument/codeAction callback
+    -- (lua/vim/lsp/handler.lua) for how to do this properly.
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, timeoutms)
+    if not result or next(result) == nil then return end
+    local actions = result[1].result
+    if not actions then return end
+    local action = actions[1]
+
+    -- textDocument/codeAction can return either Command[] or CodeAction[]. If it
+    -- is a CodeAction, it can have either an edit, a command or both. Edits
+    -- should be executed first.
+    if action.edit or type(action.command) == "table" then
+      if action.edit then
+        vim.lsp.util.apply_workspace_edit(action.edit)
       end
+      if type(action.command) == "table" then
+        vim.lsp.buf.execute_command(action.command)
+      end
+    else
+      vim.lsp.buf.execute_command(action)
+    end
   end
   
   local runtime_path = vim.split(package.path, ';')
   table.insert(runtime_path, "lua/?.lua")
   table.insert(runtime_path, "lua/?/init.lua")
   nvim_lsp.sumneko_lua.setup{
-      cmd = { "lua-language-server" },
-      settings = {
-          Lua = {
-              runtime = {
-                  -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT',
-                  -- Setup your lua path
-                  path = '/usr/local/bin/',
-              },
-              diagnostics = {
-                  -- Get the language server to recognize the `vim` global
-                  globals = {'vim'},
-              }
-          },
-          workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-          },
-          -- Do not send telemetry data containing a randomized but unique identifier
-          telemetry = {
-              enable = false,
-          },
-      } ,
+    cmd = { "lua-language-server" },
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = '/usr/local/bin/',
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        }
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    } ,
   	on_attach = on_attach,
   }
   
   local has_words_before = function ()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
   end
 end
 return M
