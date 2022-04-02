e822be2bcad419043862888f62c5b87c17f83d3c
xiwang <daring@outlook.com>
2022-04-02 22:46:06 +0800

    1, update kitty color

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,9 +1,18 @@
-hs.hotkey.bind({"option"}, "space", function()
+hs.hotkey.bind({"shift"}, "space", function()
   local alacritty = hs.application.find('alacritty')
   if alacritty ~= nil and alacritty:isFrontmost() then
     alacritty:hide()
   else
     hs.application.launchOrFocus("/Applications/Alacritty.app")
+  end
+end)
+
+hs.hotkey.bind({"option"}, "space", function()
+  local kitty = hs.application.find('kitty')
+  if kitty ~= nil and kitty:isFrontmost() then
+    kitty:hide()
+  else
+    hs.application.launchOrFocus("/Applications/kitty.app")
   end
 end)
 
