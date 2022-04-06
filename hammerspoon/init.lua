2b4c9dee9b8ef27b1a151dafc940118d50285620
xiwang <daring@outlook.com>
2022-04-06 11:52:27 +0800

    1,update color 2, disable alacritty shortcut

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,11 +1,11 @@
-hs.hotkey.bind({"shift"}, "space", function()
-  local alacritty = hs.application.find('alacritty')
-  if alacritty ~= nil and alacritty:isFrontmost() then
-    alacritty:hide()
-  else
-    hs.application.launchOrFocus("/Applications/Alacritty.app")
-  end
-end)
+-- hs.hotkey.bind({"shift"}, "space", function()
+--   local alacritty = hs.application.find('alacritty')
+--   if alacritty ~= nil and alacritty:isFrontmost() then
+--     alacritty:hide()
+--   else
+--     hs.application.launchOrFocus("/Applications/Alacritty.app")
+--   end
+-- end)
 
 hs.hotkey.bind({"option"}, "space", function()
   local kitty = hs.application.find('kitty')
