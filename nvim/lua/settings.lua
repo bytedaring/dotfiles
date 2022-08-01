59d523b9dbb3595b66c3ea78b132be3304717f5e
xiwang <daring@outlook.com>
2022-08-01 16:42:20 +0800

    1, git blame expect Trouble ;2, add yamllint config

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -111,7 +111,7 @@ opt.bg = "dark"
 --------------- Blame -----------------
 g.gitblame_message_template = '<date> • <author> • <summary>'
 g.gitblame_date_format = '%r'
-g.gitblame_ignored_filetypes = { 'NvimTree', 'packer' }
+g.gitblame_ignored_filetypes = { 'NvimTree', 'packer', 'Trouble' }
 
 --------------- Python3 -----------------
 g.python3_host_skip_check = 1
