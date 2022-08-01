eaab9e02300bf67eb6efd56b0cf364c58098fc85
xiwang <daring@outlook.com>
2022-08-01 16:21:16 +0800

    添加yamllint

--- nvim/lua/plugins/configs/lint-cfg.lua
+++ nvim/lua/plugins/configs/lint-cfg.lua
@@ -3,7 +3,8 @@ M.config = function()
   require('lint').linters_by_ft = {
     go = { 'golangcilint' },
     sh = { 'shellcheck' },
-    dockerfile = { 'hadolint' }
+    dockerfile = { 'hadolint' },
+    yaml = { 'yamllint' }
   }
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
