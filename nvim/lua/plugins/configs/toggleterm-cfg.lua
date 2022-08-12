9355e2ecebb01dc90087af64565ab39869324211
xiwang <daring@outlook.com>
2022-08-12 11:13:59 +0800

    更新terminal

--- nvim/lua/plugins/configs/toggleterm-cfg.lua
+++ nvim/lua/plugins/configs/toggleterm-cfg.lua
@@ -11,7 +11,7 @@ M.setup = function()
     insert_mappings = true, -- whether or not the open mapping applies in insert mode
     terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
     persist_size = true,
-    direction = 'float', ---'vertical' | 'horizontal' | 'window' | 'float',
+    direction = 'horizontal', ---'vertical' | 'horizontal' | 'window' | 'float',
     close_on_exit = true, -- close the terminal wndow when the process exits
     shell = vim.o.shell, -- change the default shell
     -- This field is only relevant if direction is set to 'float'
