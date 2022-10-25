6050e2b1401a8b65206bc99e32ab416ef6363fe9
xiwang <daring@outlook.com>
2022-10-25 16:38:57 +0800

    update cmp border

--- nvim/lua/plugins/configs/others.lua
+++ nvim/lua/plugins/configs/others.lua
@@ -3,14 +3,22 @@ local M = {}
 M.signature = function()
   local function border(hl_name)
     return {
-      { "╭", hl_name },
+      --   { "╭", hl_name },
+      --   { "─", hl_name },
+      --   { "╮", hl_name },
+      --   { "│", hl_name },
+      --   { "╯", hl_name },
+      --   { "─", hl_name },
+      --   { "╰", hl_name },
+      --   { "│", hl_name },
+      { "┌", hl_name },
       { "─", hl_name },
-      { "╮", hl_name },
+      { "┐", hl_name },
       { "│", hl_name },
-      { "╯", hl_name },
+      { "┘", hl_name },
       { "─", hl_name },
-      { "╰", hl_name },
-      { "│", hl_name },
+      { "└", hl_name },
+      { "│", hl_name }
     }
   end
 
