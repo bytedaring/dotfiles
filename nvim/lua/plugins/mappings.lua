b06f48d13ce1cc6e22113c6b9ee174756aaf1a3a
xiwang <daring@outlook.com>
2023-05-15 17:17:57 +0800

    update keymappig

--- nvim/lua/plugins/mappings.lua
+++ nvim/lua/plugins/mappings.lua
@@ -2,7 +2,7 @@
 -- Description: Keymaps
 --
 -- local map = vim.api.nvim_set_keymap
-local map = vim.keymap.set
+local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 -- M.symbol = function()
@@ -11,42 +11,42 @@ local opts = { noremap = true, silent = true }
 
 -- telescope
 -- Find files using Telescope command-line sugar.
-map('n', 'ff', [[<Cmd>Telescope find_files<CR>]], opts)
-map('n', 'fg', [[<Cmd>Telescope live_grep<CR>]], opts)
-map('n', 'fb', [[<Cmd>Telescope buffers<CR>]], opts)
-map('n', 'fs', [[<Cmd>Telescope grep_string<CR>]], opts)
-map('n', 'fh', [[<Cmd>Telescope help_tags<CR>]], { noremap = true, silent = true })
-map('n', 'fn', [[<Cmd>bNext<CR>]], opts)
-map('n', 'fp', [[<Cmd>bprevious<CR>]], opts)
-map('n', 'fh', [[<Cmd>Telescope oldfiles<CR>]], opts)
-map('n', 'fc', [[<Cmd>Telescope<CR>]], opts)
-map('n', 'fo', [[<Cmd>Telescope lsp_document_symbols<CR>]], opts)
-map('n', 'fr', [[<Cmd>Telescope lsp_references<CR>]], opts)
+keymap.set('n', 'ff', [[<Cmd>Telescope find_files<CR>]], opts)
+keymap.set('n', 'fg', [[<Cmd>Telescope live_grep<CR>]], opts)
+keymap.set('n', 'fb', [[<Cmd>Telescope buffers<CR>]], opts)
+keymap.set('n', 'fs', [[<Cmd>Telescope grep_string<CR>]], opts)
+keymap.set('n', 'fh', [[<Cmd>Telescope help_tags<CR>]], { noremap = true, silent = true })
+keymap.set('n', 'fn', [[<Cmd>bNext<CR>]], opts)
+keymap.set('n', 'fp', [[<Cmd>bprevious<CR>]], opts)
+keymap.set('n', 'fh', [[<Cmd>Telescope oldfiles<CR>]], opts)
+keymap.set('n', 'fc', [[<Cmd>Telescope<CR>]], opts)
+keymap.set('n', 'fo', [[<Cmd>Telescope lsp_document_symbols<CR>]], opts)
+keymap.set('n', 'fr', [[<Cmd>Telescope lsp_references<CR>]], opts)
 
 -- todo-comments.nvim
-map('n', 'ft', [[<cmd>TodoTelescope<CR>]], opts)
+keymap.set('n', 'ft', [[<cmd>TodoTelescope<CR>]], opts)
 
 -- nvim-tree NvimTreeFindFileToggle
--- nnoremap fi :<C-u>:NvimTreeFindFileToggle<CR>
-map('n', '<C-b>', [[<Cmd>NvimTreeToggle<CR>]], opts)
+-- nnorekeymap.set fi :<C-u>:NvimTreeFindFileToggle<CR>
+keymap.set('n', '<C-b>', [[<Cmd>NvimTreeToggle<CR>]], opts)
 
 -- vimspector
-map('n', '<leader>dd', [[<Cmd>:call vimspector#Launch()<CR>]], opts)
-map('n', '<leader>dx', [[<Cmd>:VimspectorReset<CR>]], opts)
-map('n', '<leader>de', [[<Cmd>:VimspectorEval<CR>]], opts)
-map('n', '<leader>dw', [[<Cm>:VimspectorWatch<CR>]], opts)
-map('n', '<leader>do', [[<Cmd>:VimspectorShowOutput<CR>]], opts)
+keymap.set('n', '<leader>dd', [[<Cmd>:call vimspector#Launch()<CR>]], opts)
+keymap.set('n', '<leader>dx', [[<Cmd>:VimspectorReset<CR>]], opts)
+keymap.set('n', '<leader>de', [[<Cmd>:VimspectorEval<CR>]], opts)
+keymap.set('n', '<leader>dw', [[<Cm>:VimspectorWatch<CR>]], opts)
+keymap.set('n', '<leader>do', [[<Cmd>:VimspectorShowOutput<CR>]], opts)
 
 -- Trouble
-map('n', '<leader>xx', [[<Cmd>TroubleToggle<CR>]], opts)
-map('n', '<leader>xw', [[<Cmd>TroubleToggle workspace_diagnostics<CR>]], opts)
-map('n', '<leader>xd', [[<Cmd>TroubleToggle document_diagnostics<CR>]], opts)
-map('n', '<leader>xq', [[<Cmd>TroubleToggle quickfix<CR>]], opts)
-map('n', '<leader>xl', [[<Cmd>TroubleToggle loclist<CR>]], opts)
+keymap.set('n', '<leader>xx', [[<Cmd>TroubleToggle<CR>]], opts)
+keymap.set('n', '<leader>xw', [[<Cmd>TroubleToggle workspace_diagnostics<CR>]], opts)
+keymap.set('n', '<leader>xd', [[<Cmd>TroubleToggle document_diagnostics<CR>]], opts)
+keymap.set('n', '<leader>xq', [[<Cmd>TroubleToggle quickfix<CR>]], opts)
+keymap.set('n', '<leader>xl', [[<Cmd>TroubleToggle loclist<CR>]], opts)
 
 -- ToggleTerm
-map('n', '<C-\\>', [[<Cmd>ToggleTerm<CR>]], opts)
-map('n', '<CS-\\>', function ()
+keymap.set('n', '<C-\\>', [[<Cmd>ToggleTerm<CR>]], opts)
+keymap.set('n', '<CS-\\>', function ()
   if vim.g.toggleTerm_direction == 'horizontal' then
     vim.cmd[[ToggleTerm direction=float]]
     vim.g.toggleTerm_direction = 'float'
@@ -55,54 +55,54 @@ map('n', '<CS-\\>', function ()
     vim.g.toggleTerm_direction = 'horizontal'
   end
 end, opts)
-vim.keymap.set('n', '<leader>s', [[<Cmd>ToggleTermSendVisualLines<CR>]], opts)
+keymap.set('n', '<leader>s', [[<Cmd>ToggleTermSendVisualLines<CR>]], opts)
 
 -- go.nvim
-map('n', '<F9>', [[<Cmd>GoDebug -b<CR>]], opts)
-map('n', '<leader>dd', [[<Cmd>GoDebug<CR>]], opts)
-map('n', '<leader>dx', [[<Cmd>GoDebug -s<CR>]], opts)
-map('n', '<leader>dt', [[<Cmd>GoDebug -t<CR>]], opts)
+keymap.set('n', '<F9>', [[<Cmd>GoDebug -b<CR>]], opts)
+keymap.set('n', '<leader>dd', [[<Cmd>GoDebug<CR>]], opts)
+keymap.set('n', '<leader>dx', [[<Cmd>GoDebug -s<CR>]], opts)
+keymap.set('n', '<leader>dt', [[<Cmd>GoDebug -t<CR>]], opts)
 
 -- vim-illuminate
-map('n', ']r', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', { noremap = true })
-map('n', '[r', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', { noremap = true })
+keymap.set('n', ']r', '<cmd>lua require"illuminate".next_reference{wrap=true}<cr>', { noremap = true })
+keymap.set('n', '[r', '<cmd>lua require"illuminate".next_reference{reverse=true,wrap=true}<cr>', { noremap = true })
 
 -- Outline
--- map("n", "<leader>o", "<cmd>AerialToggle<CR>", opts)
-map("n", "<leader>o", "<cmd>Lspsaga outline<CR>", opts)
+-- keymap.set("n", "<leader>o", "<cmd>AerialToggle<CR>", opts)
+keymap.set("n", "<leader>o", "<cmd>Lspsaga outline<CR>", opts)
 
 -- lspsaga
 -- lsp finder to find the cursor word definition and reference
-vim.keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
+keymap.set("n", "gh", "<cmd>Lspsaga lsp_finder<CR>", opts)
 -- code action
-vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
-vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", opts)
+keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", opts)
 -- code lens action
--- vim.keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.codelens.run()<CR>", opts)
-vim.keymap.set("n", "<leader>la", "<cmd>GoCodeLenAct<CR>", opts)
+-- keymap.set("n", "<leader>la", "<cmd>lua vim.lsp.codelens.run()<CR>", opts)
+keymap.set("n", "<leader>la", "<cmd>GoCodeLenAct<CR>", opts)
 -- show hover doc and press twice will jump to hover window
-vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 -- rename
-vim.keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
+keymap.set("n", "gr", "<cmd>Lspsaga rename<CR>", opts)
 -- goto definition
-vim.keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
+keymap.set("n", "gd", "<cmd>Lspsaga goto_definition<CR>", opts)
 -- peek type definition
-vim.keymap.set("n", "gP", "<cmd>Lspsaga peek_type_definition<CR>")
+keymap.set("n", "gP", "<cmd>Lspsaga peek_type_definition<CR>")
 -- peek definition
-vim.keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", opts)
+keymap.set("n", "gp", "<cmd>Lspsaga peek_definition<CR>", opts)
 -- jump diagnostic
-vim.keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-vim.keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-vim.keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+keymap.set("n", "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 
 --lazygit.nvim
-vim.keymap.set('n', '<c-g>', "<cmd>LazyGit<CR>", opts)
+keymap.set('n', '<c-g>', "<cmd>LazyGit<CR>", opts)
 
 --gx open URL in browser
-vim.keymap.set('n', 'gx', "<cmd>silent !open <cWORD><CR>", opts)
+keymap.set('n', 'gx', "<cmd>silent !open <cWORD><CR>", opts)
 
 --neotest_
-vim.keymap.set('n', '<leader>ts', [[<Cmd>NeotestToggleSummary<CR>]], opts)
-vim.keymap.set('n', '<leader>to', [[<Cmd>NeotestToggleOutputPanel<CR>]], opts)
-vim.keymap.set('n', '<leader>tr', [[<Cmd>NeotestRun<CR>]], opts)
-vim.keymap.set('n', '<leader>tf', [[<Cmd>NeotestRunFile<CR>]], opts)
+keymap.set('n', '<leader>ts', [[<Cmd>NeotestToggleSummary<CR>]], opts)
+keymap.set('n', '<leader>to', [[<Cmd>NeotestToggleOutputPanel<CR>]], opts)
+keymap.set('n', '<leader>tr', [[<Cmd>NeotestRun<CR>]], opts)
+keymap.set('n', '<leader>tf', [[<Cmd>NeotestRunFile<CR>]], opts)
